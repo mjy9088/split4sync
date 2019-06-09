e5ea1be4e06c9dd17d74ddc5504f45c227002982
맹주영 <mjy9088@naver.com>
2019-06-09 21:49:00 +0900

    join

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdint.h>
 
 int main(int argc, char **argv)
 {
@@ -8,9 +9,66 @@ int main(int argc, char **argv)
 		printf("usage :\n\t%s join filename - join spllitted files into stdout\n\t%s split filename size - split stdin to multiple files\n", argv[0], argv[0]);
 		return 1;
 	}
+	char filename[1024];
+	char buffer[1024];
+	int read;
 	if(argc == 3)
 	{
-		fputs("WIP", stderr);
+		FILE *fp = fopen(argv[2], "r");
+		if(!fp)
+		{
+			fprintf(stderr, "Failed to open file : %s\n", argv[2]);
+			return -1;
+		}
+		while(fgets(filename, 1024, fp))
+		{
+			if(ferror(fp))
+			{
+				fclose(fp);
+				fputs("Error reading file list", stderr);
+				return -2;
+			}
+			FILE *f = fopen(filename, "rb");
+			if(!f)
+			{
+				fprintf(stderr, "Failed to open file : %s\n", filename);
+				return -3;
+			}
+			while(!feof(f))
+			{
+				read = fread(buffer, sizeof(char), 1024, f);
+				if(ferror(f))
+				{
+					fclose(fp);
+					fclose(f);
+					fputs("Error reading file", stderr);
+					return -4;
+				}
+				if(fwrite(buffer, sizeof(char), read, stdout) != read || ferror(f))
+				{
+					fclose(fp);
+					fclose(f);
+					fputs("Error writing file", stderr);
+					return -5;
+				}
+			}
+			if(ferror(f))
+			{
+				fclose(fp);
+				fclose(f);
+				fputs("Error reading file", stderr);
+				return -6;
+			}
+			fclose(f);
+		}
+		if(ferror(fp))
+		{
+			fclose(fp);
+			fclose(f);
+			fputs("Error reading file list", stderr);
+			return -7;
+		}
+		fclose(fp);
 		return 0;
 	}
 	else
